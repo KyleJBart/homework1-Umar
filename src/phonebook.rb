e03ddedef1b13c627de664bfd9a9c3137fec82bb
KyleJBart <163806608+KyleJBart@users.noreply.github.com>
2025-09-17 22:55:10 -0500

    Update phonebook.rb

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,39 @@
 class PhoneBook
     def initialize
+      @entries = {}
     end
-
+  
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+      return false if @entries.key?(name)
+      return false unless number =~ /^[1-9]\d{2}-\d{3}-\d{4}$/
+      if is_listed
+        @entries.each do |_, entry|
+          return false if entry[:number] == number && entry[:listed] == true
+        end
+      end
+      @entries[name] = { number: number, listed: is_listed }
+      true
     end
-
+  
     def lookup(name)
-        raise Exception, "Not implemented"
+      return nil unless @entries.key?(name)
+      entry = @entries[name]
+      entry[:listed] ? entry[:number] : nil
     end
-
-    def lookupByNum(number)
-        raise Exception, "Not implemented"
+  
+    def lookupByNum(num)
+      @entries.each do |name, entry|
+        return name if entry[:number] == num && entry[:listed]
+      end
+      nil
     end
-
+  
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+      result = []
+      @entries.each do |name, entry|
+        result << name if entry[:number].start_with?(areacode)
+      end
+      result
     end
-end
+  end
+
