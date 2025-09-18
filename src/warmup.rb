1d2c378f0907d63e2b317dc503a5916e7e9115d6
KyleJBart <163806608+KyleJBart@users.noreply.github.com>
2025-09-17 22:54:47 -0500

    Update warmup.rb

--- src/warmup.rb
+++ src/warmup.rb
@@ -1,23 +1,38 @@
+
 def fib(n)
-    raise Exception, "Not Implemented"
+    return [] if n == 0
+    return [0] if n == 1
+  
+    res = [0, 1]
+    while res.length < n
+      res << res[-1] + res[-2]
+    end
+    res
 end
 
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+    str = n.to_s
+    str == str.reverse
 end
 
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+    res = a.sort.reverse
+    res[n]
 end
 
 def freq(s)
-    raise Exception, "Not Implemented"
+    return "" if s.empty?
+    counts = Hash.new(0)
+    s.each_char { |char| counts[char] += 1 }
+    counts.max_by { |char, count| count }[0]
 end
 
-def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+def
+    zipHash(arr1, arr2)
+    return nil unless arr1.length == arr2.length
+    Hash[arr1.zip(arr2)]
 end
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+    hash.to_a
 end
